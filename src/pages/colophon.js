7acf16ac957053de4cf16c743b16aca7694c7e13
drainpip <me@shaneis.me>
2019-01-14 11:43:31 -0800

    Add links to OSS projects

--- src/pages/colophon.js
+++ src/pages/colophon.js
@@ -8,7 +8,23 @@ const Colophon = () => (
     <SEO title="Colophon" />
     <h2>Colophon</h2>
     <p>This site is built with the help of viewers like you.</p>
-    <p>Under construction and all that...</p>
+    <p>As well as this stuff:</p>
+    <ul>
+      <li>
+        <a href="https://www.gatsbyjs.org/">Gatsby</a>
+      </li>
+      <li>
+        <a href="https://reactjs.org/">React</a>
+      </li>
+      <li>
+        <a href="https://reach.tech/router">Reach Router</a>
+      </li>
+      <li>
+        <a href="https://graphql.org/">GraphQL</a>
+      </li>
+      <li>System Fonts</li>
+      <li>Spare Time</li>
+    </ul>
   </Layout>
 )
 
