144985977c2063d6bf18842e98e5fa2fa9cf4805
Shane Duff <shaned@hey.com>
2023-01-30 15:02:42 -0800

    Run GQL codemod

--- src/components/post-index.js
+++ src/components/post-index.js
@@ -5,28 +5,23 @@ import PostList from './post-list';
 
 const PostIndex = () => (
   <StaticQuery
-    query={graphql`
-      query ListQuery {
-        allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
-          limit: 3
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                date(formatString: "MMMM Do YYYY")
-                description
-                tags
-                title
-              }
-            }
-          }
+    query={graphql`query ListQuery {
+  allMarkdownRemark(sort: {frontmatter: {date: DESC}}, limit: 3) {
+    edges {
+      node {
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM Do YYYY")
+          description
+          tags
+          title
         }
       }
-    `}
+    }
+  }
+}`}
     render={({ allMarkdownRemark }) => (
       <React.Fragment>
         <PostList posts={allMarkdownRemark} />
