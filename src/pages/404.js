d9bdbfcc4dd13071a4ae346360ba0271e428bd0d
Shane Duff <shaned@hey.com>
2023-09-29 11:10:37 -0700

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import TagsList from '../components/tags-list';
 import Seo from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h2>Link Does Not Exist</h2>
-    <p>
-      Go <Link to="/">home</Link>.
-    </p>
+    <h2>Quoth the server: 404</h2>
+    <p>Try to find what you're looking for with these links:</p>
+    <TagsList />
   </Layout>
 );
 
