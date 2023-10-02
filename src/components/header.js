062af8030dae958a5649b101e7c0ce3d503d6f39
Shane Duff <shaned@hey.com>
2023-10-02 16:53:23 -0700

    Redesigned header & home page links

--- src/components/header.js
+++ src/components/header.js
@@ -4,30 +4,40 @@ import PropTypes from 'prop-types';
 import { css, jsx } from '@emotion/react';
 
 const header = css`
-  background-color: #555;
-  text-align: center;
+  max-width: 40em;
+  margin-bottom: 1em;
+  margin-left: 1.5em;
+  margin-right: 1.5em;
+  margin-top: 1em;
 
-  @media screen and (prefers-color-scheme: dark) {
-    background-color: #999;
+  @media (min-width: 40em) {
+    margin-left: auto;
+    margin-right: auto;
   }
 `;
 
 const headerHeading = css`
+  background-color: #555;
+  box-shadow: 0.5rem 0.5rem #222;
   line-height: 1;
   margin-bottom: 0;
-  padding-bottom: 0.15em;
-  padding-top: 0.15em;
+
+  @media screen and (prefers-color-scheme: dark) {
+    background-color: #999;
+    box-shadow-color: #000;
+  }
 `;
 
 const headerLink = css`
   color: #fff;
-  border-bottom: 5px solid;
+  display: block;
+  padding: 0.15em 0.25em;
   text-decoration: none;
   &:visited {
     color: #fff;
   }
   &:hover {
-    color: rgba(255, 255, 255, 0.8);
+    color: rgba(255, 255, 255, 0.2);
   }
 
   @media screen and (prefers-color-scheme: dark) {
@@ -41,11 +51,23 @@ const headerLink = css`
   }
 `;
 
+const blinkingCursor = css`
+  position: relative;
+  top: -0.1em;
+
+  @keyframes cursor-blink { 
+    0% {
+      opacity: 0;
+    }
+  }
+  animation: cursor-blink 1.5s steps(2) infinite;
+`
+
 const Header = ({ siteTitle }) => (
   <header css={header}>
     <h1 css={headerHeading}>
       <Link to="/" css={headerLink}>
-        {siteTitle}
+        {siteTitle}<span css={blinkingCursor}>|</span>
       </Link>
     </h1>
   </header>
